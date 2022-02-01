3dc8f32169915159e1c10ea8d1f4722ab2ca86b4
Hodan <you@example.com>
2022-02-01 06:04:29 -0500

    started routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,44 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+const { sequelize } = require("../../models/Category");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
+  Category.findAll()
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status;
+    });
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "category_name",
+      [sequelize.literal("SELECT (*) FROM category WHERE ")],
+    ],
+  });
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
